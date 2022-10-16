c9161f90ac6c2a9be4606d6aaf8f3fd8815b7438
xd-hearst <xun.ding@cds-global.com>
2022-10-16 00:12:24 -0500

    preformance test

---
+++ pages/scripts-demo/performace/next-script.js
@@ -0,0 +1,53 @@
+import Script from 'next/script';
+
+export default function NextScriptTest() {
+	return (
+		<>
+			<div>
+				<div class="css-122y91a">
+					<img
+						src="https://static01.nyt.com/images/2022/10/18/science/14tb-sarcastic-fringehead-vid-image/14tb-sarcastic-fringehead-vid-image-superJumbo.jpg"
+						alt="Cinemagraph"
+					/>
+					<p>
+						Fusce pharetra ligula id arcu semper, ac laoreet risus pretium.
+						Morbi vitae mi quis urna cursus ultricies. Aliquam varius tristique
+						efficitur. Nulla venenatis lobortis sapien quis scelerisque. Proin
+						interdum nunc a lacus interdum dignissim. Orci varius natoque
+						penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+						Nulla facilisi. Nam magna magna, placerat in malesuada sit amet,
+						blandit vel mauris. Donec vel tortor aliquam, iaculis magna ac,
+						feugiat felis. Mauris porttitor at tortor a fringilla. Integer
+						auctor, urna sit amet volutpat blandit, eros risus tincidunt libero,
+						at tristique sapien velit eu purus. Donec in laoreet sapien.
+						Vestibulum accumsan ullamcorper vehicula. Aliquam tincidunt volutpat
+						tincidunt. Nunc eu risus non magna fringilla tristique ac ultricies
+						dui. Proin eget pretium arcu. Nulla consequat in ex id porttitor.
+						Donec vehicula lacinia tincidunt. Cras auctor sollicitudin nisl, sed
+						porta lorem convallis eget. Mauris eget lectus fermentum, tempus
+						nunc vitae, commodo turpis. Mauris consectetur blandit nisi viverra
+						pulvinar. Proin viverra diam nec ipsum feugiat, accumsan fringilla
+						lectus tempor. Duis at sapien tellus. Mauris a accumsan ex. Nam
+						lorem turpis, ornare quis commodo id, consequat non ipsum. Morbi
+						lobortis sed lacus id porttitor. Aliquam ultrices cursus
+						sollicitudin. Fusce nec nisl velit. Praesent eget tempus tellus. Ut
+						a turpis lacinia, consequat nisi ut, efficitur nisl. Phasellus ac
+						pretium diam. Ut risus lacus, placerat ut viverra a, fermentum sit
+						amet ligula. Sed quis suscipit dolor. Praesent id egestas leo, et
+						maximus libero. Mauris quis varius ligula. Nam vitae massa
+						hendrerit, eleifend turpis a, feugiat arcu. Integer eu augue tellus.
+					</p>
+					<video
+						data-testid="cinemagraph"
+						src="https://vp.nyt.com/video/2022/10/13/103049_1_14tb-sarcastic-fringehead-vid_wg_720p.mp4"
+						muted=""
+						loop=""
+						autoplay=""
+						playsinline=""
+					></video>
+				</div>
+			</div>
+			<Script src="https://www.google-analytics.com/analytics.js" />
+		</>
+	);
+}
